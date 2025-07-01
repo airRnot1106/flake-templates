fbd3c28719ba3b519b6318c51b2fc1b8add9d63c
airRnot1106 <yngw.rintaro@gmail.com>
2025-07-01 10:45:59 +0900

    feat: add rust template

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "Collection of Nix Flakes templates";
-
   inputs = {
     devenv.url = "github:cachix/devenv";
     git-hooks = {
@@ -8,6 +6,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     systems.url = "github:nix-systems/default";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
@@ -37,9 +39,13 @@
           devenv.shells.default = {
             packages = with pkgs; [
               git
+              nil
             ];
             containers = lib.mkForce { };
-            languages.nix.enable = true;
+            languages.rust = {
+              enable = true;
+              channel = "nightly";
+            };
             enterShell = ''
               ${config.pre-commit.installationScript}
             '';
@@ -48,14 +54,5 @@
           pre-commit = import ./nix/pre-commit { inherit config; };
           treefmt = import ./nix/treefmt;
         };
-
-      flake = {
-        templates = {
-          rust = {
-            path = ./templates/rust;
-            description = "Rust project template";
-          };
-        };
-      };
     };
 }
